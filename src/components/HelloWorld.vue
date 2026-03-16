9ebab68d604f561a7e9c476556a5060126070bbe
Sobanamjad <sobanamjad097@gmail.com>
2026-03-16 11:34:38 +0500

    feat:Create sginup page

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style scoped></style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import axios from "axios";
+const name = ref("");
+const email = ref("");
+const password = ref("");
+
+const signup = async () => {
+  console.warn("Sign up Success", name.value, email.value, password.value);
+  let result = await axios.post("http://localhost:3000/users", {
+    email: email.value,
+    password: password.value,
+    name: name.value,
+  });
+  console.warn("result", result);
+  if (result.status == 201) {
+    alert("Sign up Success");
+  }
+  localStorage.setItem("user-info", JSON.stringify(result.data));
+};
+</script>
+<template>
+  <div class="signup">
+    <img class="logo" src="../assets/resto.jpg" alt="" />
+    <h1>Sign Up Component</h1>
+    <div class="signup-form">
+      <label class="input-label" for="Name">Name: </label>
+      <input
+        class="input-field"
+        type="text"
+        placeholder="Enter Name"
+        v-model="name"
+      />
+      <br />
+      <br />
+
+      <label class="input-label" for="Name">Email: </label>
+      <input
+        class="input-field"
+        type="text"
+        placeholder="Enter Email"
+        v-model="email"
+      />
+      <br />
+      <br />
+
+      <label class="input-label" for="Name">Password: </label>
+      <input
+        class="input-field"
+        type="password"
+        placeholder="Enter Password"
+        v-model="password"
+      />
+      <br />
+      <br />
+      <button class="signup-btn" v-on:click="signup">Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.signup {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10px;
+  padding-top: 10px;
+}
+.logo {
+  width: 100px;
+  border-radius: 10px;
+}
+.input-label {
+  width: 100px;
+  display: inline-block;
+  font-size: 16px;
+  font-weight: bold;
+}
+.input-field {
+  width: 200px;
+  height: 30px;
+  border: 1px solid skyblue;
+  border-radius: 5px;
+  padding: 5px;
+}
+.signup-btn {
+  display: absolute;
+  margin-top: 10px;
+  width: 200px;
+  height: 40px;
+  background-color: skyblue;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+}
+</style>
